f8873388ac8b89497c4551e8ffe7d8b563da6586
Simon Thörnqvist <simon.thornqvist@gmail.com>
2021-03-06 17:40:10 +0100

    insert, update, delete placeholder

--- fdw_server/src/main.rs
+++ fdw_server/src/main.rs
@@ -1,6 +1,6 @@
 use futures::Stream;
 use pg::fdw_server::{Fdw, FdwServer};
-use pg::{ExecuteRequest, ResultSet};
+use pg::{DeleteRequest, ExecuteRequest, InsertRequest, ResultSet, UpdateRequest};
 use prost_types::Value;
 use std::pin::Pin;
 use std::sync::Arc;
@@ -21,9 +21,18 @@ impl Fdw for EchoFdw {
     type ExecuteStream =
         Pin<Box<dyn Stream<Item = Result<ResultSet, Status>> + Send + Sync + 'static>>;
 
+    type InsertStream =
+        Pin<Box<dyn Stream<Item = Result<ResultSet, Status>> + Send + Sync + 'static>>;
+
+    type UpdateStream =
+        Pin<Box<dyn Stream<Item = Result<ResultSet, Status>> + Send + Sync + 'static>>;
+
+    type DeleteStream =
+        Pin<Box<dyn Stream<Item = Result<ResultSet, Status>> + Send + Sync + 'static>>;
+
     async fn execute(
         &self,
-        request: Request<ExecuteRequest>,
+        _request: Request<ExecuteRequest>,
     ) -> Result<Response<Self::ExecuteStream>, Status> {
         let (tx, rx) = mpsc::channel(4);
         let rows = self.rows.clone();
@@ -33,6 +42,42 @@ impl Fdw for EchoFdw {
                 tx.send(Ok(row.clone())).await.unwrap()
             }
         });
+
+        Ok(Response::new(Box::pin(
+            tokio_stream::wrappers::ReceiverStream::new(rx),
+        )))
+    }
+
+    async fn insert(
+        &self,
+        _request: Request<InsertRequest>,
+    ) -> Result<tonic::Response<Self::InsertStream>, Status> {
+        let (_tx, rx) = mpsc::channel(4);
+        let mut _rows = &self.rows;
+
+        Ok(Response::new(Box::pin(
+            tokio_stream::wrappers::ReceiverStream::new(rx),
+        )))
+    }
+
+    async fn update(
+        &self,
+        _request: Request<UpdateRequest>,
+    ) -> Result<tonic::Response<Self::UpdateStream>, Status> {
+        let (_tx, rx) = mpsc::channel(4);
+        let mut _rows = &self.rows;
+
+        Ok(Response::new(Box::pin(
+            tokio_stream::wrappers::ReceiverStream::new(rx),
+        )))
+    }
+
+    async fn delete(
+        &self,
+        _request: Request<DeleteRequest>,
+    ) -> Result<Response<Self::DeleteStream>, tonic::Status> {
+        let (_tx, rx) = mpsc::channel(4);
+        let mut _rows = &self.rows;
 
         Ok(Response::new(Box::pin(
             tokio_stream::wrappers::ReceiverStream::new(rx),
